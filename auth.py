693a3e7d9c3addc2358f0358df5d53a30c5685ba
purti mishra <kanakkk789@users.noreply.huggingface.co>
2026-03-06 13:17:03 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,6 +3,8 @@ import jwt
 import datetime
 import os
 from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 load_dotenv()
 
@@ -28,3 +30,21 @@ def verify_jwt(token):
         return decoded
     except:
         return None
+
+load_dotenv()
+
+def send_otp_via_sendgrid(receiver_email, otp):
+    message = Mail(
+        from_email='your-verified-sender@example.com', # Must match SendGrid settings
+        to_emails=receiver_email,
+        subject='Your FitPlan AI OTP',
+        html_content=f'<strong>Your verification code is: {otp}</strong>'
+    )
+    
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        return response.status_code == 202 # 202 is the successful code
+    except Exception as e:
+        print(f"SendGrid Error: {e}")
+        return False
